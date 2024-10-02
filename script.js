5d5e2c9e1e57198e8121395fd98d4463eead812b
Neeraj Shrivas <shrivasneeraj01@gmail.com>
2024-10-02 20:24:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function volume_sphere() {
     //Write your code here
-  
+	let radius = document.getElementById("radius");
+	let volume = (4/3) * Math.PI * Math.pow(radius.value, 3);
+	if(radius.value < 0 || isNaN(radius.value)) {
+		document.getElementById("volume").value = 'NaN';
+		return;
+	} else {
+		document.getElementById("volume").value = volume.toFixed(4);
+	}
+	
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = (e) => {
+	e.preventDefault();
+	volume_sphere();
+};
